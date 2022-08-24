fed7dcff4b8d0d28212d3a39950090d738086789
Yuvraj Singh <111690433+yuvraj-31@users.noreply.github.com>
2022-08-24 18:32:12 +0530

    Update main 5.c

--- main 5.c
+++ main 5.c
@@ -7,6 +7,7 @@ int main()
     printf("Enter the value of N\n");
     scanf("%d",&n);
     printf("the first %d odd natural numbers in reverse order are:\n",n);
+    n=n*2;
     while(n>=1)
     {   if((n%2)!=0)
         {
